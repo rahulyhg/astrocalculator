237e8744ff3aaff098415aefe333b6df0995492d
coderchira <ruchira.aggarwal@gmail.com>
2016-04-17 17:34:08 +0530

    added angular app

---
+++ astrocalculator/astrocalc.web/ng/modules/astroapp.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    var astroapp = angular.module("astroapp", ["ngRoute"])
+        .config(["$routeProvider", function ($routeProvider) {
+            $routeProvider
+            .when("/", { templateUrl: "ng/views/home.html" })
+            .when("/home", { templateUrl: "ng/views/home.html" })
+            .otherwise({ redirectTo: "/" })
+        }])
+})()
