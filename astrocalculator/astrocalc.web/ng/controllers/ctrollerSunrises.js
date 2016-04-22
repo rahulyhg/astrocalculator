0cd94c039f656d0abb4ba9bc6c3176ee43405baa
kneerunjun <kneerunjun@gmail.com>
2016-04-22 13:36:39 +0530

    able to get / set the solar configuration for the request thru user input

--- astrocalculator/astrocalc.web/ng/controllers/ctrollerSunrises.js
+++ astrocalculator/astrocalc.web/ng/controllers/ctrollerSunrises.js
@@ -1,20 +1,42 @@
 ﻿(function () {
     var ctrollerSunrises = angular.module("astroapp").controller("ctrollerSunrises", function ($scope, svcWebApi) {
         $scope.model = {};
-        
-        console.info("we are inside the sunrises controller");
+        $scope.selection = {
+            city :"",year:0, month:""
+        };
         var init = function () {
             //this is where you can go ahead to get the data from the server
             //data  - solar configuration and the options
-            console.info("now getting the colar configuration");
             svcWebApi.getSolarConfig().then(function (data) {
                 $scope.model = angular.copy(data, $scope.model);
-                $scope.model.city = $scope.model.citychoices[0].title;
+                $scope.selection.city = $scope.model.citychoices[0].title;
+                $scope.selection.year = $scope.model.yearchoices[0].toString();
+                $scope.selection.month = $scope.model.monthchoices[0];
             }, function (status) {
                 //this is when the model was not being populated .. 
                 $scope.model = {};
             })
         }; init();
-      
+        $scope.$watch("selection.city", function (after, before) {
+            if (after !== undefined && after != null && after != "" && $scope.model.citychoices!==undefined) {
+                $scope.model.city = $scope.model.citychoices.filter(function (el) {
+                    return el.title == after;
+                })[0];
+            }
+        })
+        $scope.$watch("selection.year", function (after, before) {
+            if (after !== undefined && after != null && $scope.model.yearchoices!==undefined) {
+                $scope.model.year = $scope.model.yearchoices.filter(function (el) {
+                    return el == after;
+                })[0];
+            }
+        });
+        $scope.$watch("selection.month", function (after, before) {
+            if (after !== undefined && after != null && after != "" && $scope.model.monthchoices!==undefined) {
+                $scope.model.month = $scope.model.monthchoices.filter(function (el) {
+                    return el == after;
+                })[0];
+            }
+        })
     })
 })()
