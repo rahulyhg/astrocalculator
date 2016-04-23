d7a9dab288ad0c40600021607db73d94842243d7
kneerunjun <kneerunjun@gmail.com>
2016-04-23 07:34:16 +0530

    code needs refactor , basic functions done

--- astrocalculator/astrocalc.web/ng/controllers/ctrollerSunrises.js
+++ astrocalculator/astrocalc.web/ng/controllers/ctrollerSunrises.js
@@ -5,6 +5,19 @@
             city :"",year:0, month:""
         };
         $scope.solardetails = [];
+        var getSolarDetails = function () {
+            var postmodel = {};
+            postmodel = angular.copy($scope.model, postmodel);
+            postmodel.choices = null;
+            postmodel.zenith = 82.3;
+            svcWebApi.getSunrises(postmodel).then(function (data) {
+                console.log("we have received the monthly solar details .. ");
+                $scope.solardetails = data;
+                console.debug($scope.solardetails);
+            }, function (status) {
+                console.error("failed to receive the monthly solar details");
+            })
+        }
         var init = function () {
             //this is where you can go ahead to get the data from the server
             //data  - solar configuration and the options
@@ -13,17 +26,7 @@
                 $scope.selection.city = $scope.model.city.title;
                 $scope.selection.year = $scope.model.year.toString();
                 $scope.selection.month = $scope.model.choices.month[$scope.model.month - 1];
-                //with the current month , day year and the location we can make the call to get the sunrises
-                var postmodel = {};
-                postmodel = angular.copy($scope.model, postmodel);
-                postmodel.choices = null;
-                svcWebApi.getSunrises(postmodel).then(function (data) {
-                    console.log("we have received the monthly solar details .. ");
-                    $scope.solardetails = data;
-                    console.debug($scope.solardetails);
-                }, function (status) {
-                    console.error("failed to receive the monthly solar details");
-                })
+                getSolarDetails();
             }, function (status) {
                 //this is when the model was not being populated .. 
                 $scope.model = {};
@@ -33,21 +36,28 @@
             if (after !== undefined && after != null && after != "" && $scope.model.choices!==undefined) {
                 $scope.model.city = $scope.model.choices.city.filter(function (el) {
                     return el.title == after;
-                })[0];
+                    //this also would mean we can get the solar details now 
+                   
+                })[0]; getSolarDetails();
             }
         })
         $scope.$watch("selection.year", function (after, before) {
             if (after !== undefined && after != null && $scope.model.choices!==undefined) {
                 $scope.model.year = $scope.model.choices.year.filter(function (el) {
                     return el == after;
-                })[0];
+                    //year has changed so that we can get the solar details
+                   
+                })[0]; getSolarDetails();
             }
         });
         $scope.$watch("selection.month", function (after, before) {
-            if (after !== undefined && after != null && after != "" && $scope.model.choices!==undefined) {
-                $scope.model.month = $scope.model.choices.month.filter(function (el) {
+            if (after !== undefined && after != null && after != "" && $scope.model.choices !== undefined) {
+                var monthName= $scope.model.choices.month.filter(function (el) {
                     return el == after;
+                    //the month has changed so we can now get the solar details
                 })[0];
+                $scope.model.month = $scope.model.choices.month.indexOf(monthName)+1;
+                getSolarDetails();
             }
         })
     })
