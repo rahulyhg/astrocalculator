c44d7b26e01fc320d31a801eb5d73e200a379e75
kneerunjun <kneerunjun@gmail.com>
2016-04-26 14:40:05 +0530

    code fix for #6

--- astrocalculator/astrocalc.web/ng/controllers/ctrollerEphemeris.js
+++ astrocalculator/astrocalc.web/ng/controllers/ctrollerEphemeris.js
@@ -2,13 +2,17 @@
     var ctrollerEphemeris = angular.module("astroapp").controller("ctrollerEphemeris", function ($scope, svcWebapi) {
         $scope.cityLike = "";
         $scope.cities = [];
+        //this is the model that is used to send to api to get the ephemeris values
+        $scope.httpBody = {
+            lat: null,
+            lng: null,
+            zen: null,
+            yr: null,
+            mn:null
+        }
         var citySuggestions = function (like) {
             svcWebapi.citySuggestions(like).then(function (data) {
-               
-                data.forEach(function (el, idx) {
-                    el.picked = false;
-                    $scope.cities.push(el)
-                })
+                $scope.cities = data;//whatever the server sends in is an acceptable model
             })
         }
         $scope.$watch("cityLike", function (after, before) {
@@ -18,15 +22,23 @@
                 citySuggestions(after);
             }
         });
+
         $scope.selectCity = function (s) {
-            $scope.cities.filter(function (el, index) {
-                return el.id != s.id; //we are trying to get all the other cities not being selected
-            }).forEach(function (el, index) {
-                el.picked = false;
-            });
-            $scope.cities.filter(function (el, index) {
+            //trying to get the city from the collection
+            var city  = $scope.cities.filter(function (el, index) {
                 return el.id == s.id;
-            })[0].picked = true;
+            })[0];
+            //getting the request payload populated.
+            if (city != null && city !== undefined) {
+                $scope.httpBody.lat = city.latitude;
+                $scope.httpBody.lng = city.longitude;
+            }
+            //clearing off the suggestions since the city has been selected
+            $scope.cities = [];
+        }
+        $scope.deselectCity = function () {
+            $scope.httpBody.lat = null;
+            $scope.httpBody.lng = null;
         }
     })
 })();
