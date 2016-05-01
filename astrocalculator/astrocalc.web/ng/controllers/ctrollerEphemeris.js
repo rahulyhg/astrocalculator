0ea130b1daf057c54a2d6903567e3259b37ec854
kneerunjun <kneerunjun@gmail.com>
2016-05-01 20:27:08 +0530

    working version of the ephemeris

--- astrocalculator/astrocalc.web/ng/controllers/ctrollerEphemeris.js
+++ astrocalculator/astrocalc.web/ng/controllers/ctrollerEphemeris.js
@@ -2,11 +2,46 @@
     var ctrollerEphemeris = angular.module("astroapp").controller("ctrollerEphemeris", function ($scope, svcWebapi, $location) {
         $scope.dt = null;
         $scope.location = null;
-        $scope.meridian = {
-            title: "",
-            degrees:null,
+        $scope.meridian = null;
+        $scope.ephemeris = [];
+        var checkIfOkForDownload = function () {
+            return $scope.dt !== null &&
+                $scope.location.lat !== null &&
+                $scope.location.lng !== null &&
+                $scope.meridian != null;
         }
-       
-       
+        var downloadEphemeris = function () {
+            svcWebapi.getSolarEphemeris($scope.location.lat,
+                $scope.location.lng,
+                $scope.meridian.value,
+                $scope.dt.year,
+                $scope.dt.month).then(function (data) {
+                    $scope.ephemeris = data;
+            });
+        }
+        $scope.$watch("dt", function (after, before) {
+            if (checkIfOkForDownload() == true) {
+                downloadEphemeris();
+            }
+            else {
+                $scope.ephemeris = [];
+            }
+        },true);
+        $scope.$watch("location", function (after, before) {
+            if (checkIfOkForDownload() == true) {
+                downloadEphemeris();
+            }
+            else {
+                $scope.ephemeris = [];
+            }
+        }, true);
+        $scope.$watch("meridian", function (after, before) {
+            if (checkIfOkForDownload() == true) {
+                downloadEphemeris();
+            }
+            else {
+                $scope.ephemeris = [];
+            }
+        });
     })
 })();
---
+++ astrocalculator/astrocalc.web/ng/directives/option-picker.js
@@ -0,0 +1,47 @@
+﻿(function () {
+    var optionPicker = angular.module("astroapp").directive("optionPicker", function ($injector) {
+        return {
+            restrict: "EA",
+            scope: {
+                option:"="
+            },
+            templateUrl: "ng/templates/option-picker.html",
+            controller: function ($scope) {
+                $scope.options = [];
+                $scope.dropMenu = false;
+               
+                $scope.$watch("fn", function (after, before) {
+                    if (after != null && after !== undefined) {
+                        //here we can get the options for the control
+                        $scope.fn().then(function (data) {
+                            $scope.options = data;
+                            $scope.options.forEach(function (el) {
+                                el.sel = false;
+                            });
+                            if ($scope.options!==null && $scope.options.length>0) {
+                               $scope.selectOption( $scope.options[0]);
+                            }
+                        })
+                    }
+                });
+                $scope.selectOption = function (option) {
+                    $scope.option = option;
+                    $scope.options.forEach(function (el) {
+                        if (el.id == option.id) { el.sel = true; }
+                        else { el.sel = false; }
+                    });
+                    $scope.dropMenu = false;
+                }
+            },
+            link: function (scope, elem, attrs) {
+                var service_fn = attrs.srv;
+                if (service_fn != "" && service_fn!=null) {
+                    var service = $injector.get(service_fn.split('.')[0]);
+                    if (service != null) {
+                        scope.fn = service[service_fn.split('.')[1]];
+                    }
+                }
+            }
+        }
+    })
+})()
